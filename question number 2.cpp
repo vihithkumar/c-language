8ca10ca120091a5c8645ee205dfbba9a76a51d5e
vihithkumar <123449989+vihithkumar@users.noreply.github.com>
2023-04-04 19:07:03 +0530

    Add files via upload

---
+++ question number 2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (num % 5 == 0 && num % 11 == 0) {
+        printf("%d is divisible by both 5 and 11.\n", num);
+    } else if (num % 5 == 0) {
+        printf("%d is divisible by 5.\n", num);
+    } else if (num % 11 == 0) {
+        printf("%d is divisible by 11.\n", num);
+    } else {
+        printf("%d is not divisible by either 5 or 11.\n", num);
+    }
+    
+    return 0;
+}
